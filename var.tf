9ed3d6d473838fad84cf15883f5f2b37cce792ac
amitvm <53211276+amitvm@users.noreply.github.com>
2022-04-01 20:23:31 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -18,6 +18,9 @@ variable "password" {
 variable "allocated_storage" {
      default = 10
  }
+variable "vpc_id" {
+     default = "will be apply"
+ }
 variable "engine" {
      default = "mysql"
  }
