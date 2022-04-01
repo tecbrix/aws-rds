8e99423b0d4168eda64f86e6c8fd9ce4802b6a6b
amitvm <53211276+amitvm@users.noreply.github.com>
2022-04-01 22:07:36 +0530

    Update rds.tf

--- rds.tf
+++ rds.tf
@@ -3,6 +3,7 @@ resource "aws_db_instance" "default" {
   engine                 = var.engine
   engine_version         = var.engine_version
   instance_class         = var.instance_class
+  identifier             = var.name
   db_name                = var.db_name
   username               = var.username
   password               = var.password
